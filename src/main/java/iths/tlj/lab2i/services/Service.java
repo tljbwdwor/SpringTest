9bda67b1916ac8d4ed8580b37ef9dccb68b0940d
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-02-22 15:31:05 +0100

    added services interface for tests

--- src/main/java/iths/tlj/lab2i/services/Service.java
+++ src/main/java/iths/tlj/lab2i/services/Service.java
@@ -1,5 +1,26 @@
-/*package iths.tlj.lab2i.services;
+package iths.tlj.lab2i.services;
+
+import iths.tlj.lab2i.dtos.FirstNameDto;
+import iths.tlj.lab2i.dtos.GuitaristDto;
+import iths.tlj.lab2i.dtos.LastNameDto;
+import iths.tlj.lab2i.dtos.NationalityDto;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface Service {
 
-}*/
+    GuitaristDto createGuitarist(GuitaristDto guitarist);
+
+    List<GuitaristDto> getAllGuitarists();
+
+    Optional<GuitaristDto> getSingleGuitarist();
+
+    GuitaristDto replace(int id, GuitaristDto guitaristDto);
+
+    GuitaristDto updateFirst(int id, FirstNameDto firstNameDto);
+    GuitaristDto updateLast(int id, LastNameDto lastNameDto);
+    GuitaristDto updateNationality(int id, NationalityDto nationalityDto);
+
+    void delete(int id);
+}
